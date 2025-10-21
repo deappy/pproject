898467af09119512f8fbf3a63122da6d61e733be
Akshay Raju <166707593+deappy@users.noreply.github.com>
2025-10-21 13:24:22 +0530

    inputs

---
+++ tipCalculator.py
@@ -0,0 +1,4 @@
+print("Welcome to Tip Calculator")
+bill = float(input("Enter the total bill: "))
+tip = int(input("What percentage of the tip you would like to offfer (10 12 15) : "))
+people = int(input("How many people to split the bill? "))
