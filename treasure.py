ed768b2db40ed3feac12e2c935d230b871e63194
Akshay Raju <166707593+deappy@users.noreply.github.com>
2025-10-25 23:15:01 +0530

    phase1

--- treasure.py
+++ treasure.py
@@ -1,14 +1,24 @@
-print("==========================================")
-print(r'''
- _                                     
-| |                                    
-| |_ _ __ ___  __ _ ___ _   _ _ __ _____ 
-| __| '__/ _ \/ _` / __| | | | '__/ _  /
-| |_| | |  __/ (_| \__ \ |_| | | |  __/
- \__|_|  \___|\__,_|___/\__,_|_|  \___| ''')
+import os
+terminal_width = os.get_terminal_size().columns
 
+print("=======================================================================================".center(terminal_width))
+
+text = r'''
+ _
+| |
+| |_ _ __ ___  __ _ ___ _   _ _ __ ___
+| __| '__/ _ \/ _` / __| | | | '__/ _ \
+| |_| | |  __/ (_| \__ \ |_| | | |  __/
+ \__|_|  \___|\__,_|___/\__,_|_|  \___| 
+'''
+
+print(text.center(terminal_width))
 print('\n\n\n')
 
-print("==========================================")
-print("\tWelcome to TREASURE ISLAND!")
-print("==========================================")
+print("=======================================================================================".center(terminal_width))
+print("Welcome to TREASURE ISLAND!".center(terminal_width))
+print("=======================================================================================".center(terminal_width))
+
+print('You are required to reach the island to acquire wealth from TREASURE!')
+print("=======================================================================================".center(terminal_width))
+choice1 = input("You are required to choose \"left\" or \"right\". Enter your choice!").lower()
